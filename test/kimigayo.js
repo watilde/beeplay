52035c528b895d96095f7657946c57f1ba2553eb
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-04-15 20:03:14 +0900

    Add node modules

---
+++ test/kimigayo.js
@@ -0,0 +1,162 @@
+module.exports = [
+{
+  number: 'D4',
+  length: 1
+},
+{
+  number: 'C4',
+  length: 1
+},
+{
+  number: 'D4',
+  length: 1
+},
+{
+  number: 'E4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'E4',
+  length: 1
+},
+{
+  number: 'D4',
+  length: 2
+},
+{
+  number: 'E4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 0.5
+},
+{
+  number: 'A4',
+  length: 0.5
+},
+{
+  number: 'D5',
+  length: 1
+},
+{
+  number: 'B4',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'E4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 2
+},
+{
+  number: 'D5',
+  length: 1
+},
+{
+  number: 'C5',
+  length: 1
+},
+{
+  number: 'D5',
+  length: 2
+},
+{
+  number: 'E4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'E4',
+  length: 1.5
+},
+{
+  number: 'G4',
+  length: 0.5
+},
+{
+  number: 'D4',
+  length: 2
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'C5',
+  length: 1
+},
+{
+  number: 'D5',
+  length: 2
+},
+{
+  number: 'C5',
+  length: 1
+},
+{
+  number: 'D5',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 1
+},
+{
+  number: 'A4',
+  length: 1
+},
+{
+  number: 'G4',
+  length: 0.5
+},
+{
+  number: 'E4',
+  length: 0.5
+},
+{
+  number: 'D4',
+  length: 2
+}
+];
