ac085303dab8436c685c06c3bb5db0d48248fc29
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-04-29 19:52:46 +0900

    Update beeplay class

--- browser/src/main.js
+++ browser/src/main.js
@@ -1,2 +1,9 @@
-var b = beeplay({ bpm: 80})
-  .play('D4', 100);
+var b = beeplay({ bpm: 120})
+  .play('C4', 1)
+  .play('D4', 1)
+  .play('E4', 1)
+  .play('F4', 1)
+  .play('G4', 1)
+  .play('A4', 1)
+  .play('B4', 1)
+  .play('C5', 1);
