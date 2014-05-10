61cf9d9cc9bb9f7527a58885374215bd24919250
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-05-10 15:17:50 +0900

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-nyan({bpm: 120});
-
 $(function(){
   window.prettyPrint();
+  setPlay();
+  $('#code').css('opacity', '0.2');
+  var singleton = true;
+  $('#play').on('click', function () {
+    if (singleton) {
+      singleton = false;
+      $('#play').fadeOut('fast', function () {
+        $('#code').animate({opacity: '1'}, 'slow', function () {
+          nyan({bpm: 120});
+        });
+      });
+    }
+  });
 });
+$(window).resize(setPlay);
+
+function setPlay () {
+  var code = $('#code');
+  var play = $('#play');
+  var ctop = code.offset().top;
+  var cleft = code.offset().left;
+  var cwidth = parseInt(code.css('width'));
+  var cheight = parseInt(code.css('height'));
+  var pwidth = parseInt(play.css('width'));
+  var pheight = parseInt(play.css('height'));
+
+  play.css('top', ctop + (cheight / 2) - (pheight / 2));
+  play.css('left', cleft + (cwidth / 2) - (pwidth / 2));
+}
