3d4d564cd0315b7a09cc99e9566f6be3ef101144
Luke Horvat <lukehorvat@gmail.com>
2014-11-11 14:03:34 +1100

    Eliminated "that" variable

--- src/modules/watch.js
+++ src/modules/watch.js
@@ -1,12 +1,11 @@
 module.exports = function() {
   if(this.track && typeof this.track == 'function') {
-    var that = this;
     setTimeout(function() {
-      var notes = that.stack[that.trackId].notes.toString().replace(/,/g, ' ') || 'null';
-      that.track(notes, that.trackId, that.stack);
-      that.trackId++;
-    }, that.currentTime * 1000);
+      var notes = this.stack[this.trackId].notes.toString().replace(/,/g, ' ') || 'null';
+      this.track(notes, this.trackId, this.stack);
+      this.trackId++;
+    }.bind(this), this.currentTime * 1000);
   } else {
     this.watch = function(){};
   }
-}
+};
