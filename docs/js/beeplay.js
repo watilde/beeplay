f4d4a5492dc14e02b1f2d307805dca0205d7bc0d
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-02-28 21:32:27 +0100

    support Node.js

--- docs/js/beeplay.js
+++ docs/js/beeplay.js
@@ -1,5 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-window.beeplay = function (option) {
+(function (global){
+global.beeplay = module.exports = function (option) {
   'use strict';
 
   var beeplay               = require('./modules/main');
@@ -16,6 +17,7 @@ window.beeplay = function (option) {
   return new beeplay(option);
 };
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./modules/isArray":2,"./modules/main":3,"./modules/nn":4,"./modules/pd":5,"./modules/play":6,"./modules/pn":7,"./modules/put":8,"./modules/start":9,"./modules/toJSON":10,"./modules/watch":11}],2:[function(require,module,exports){
 module.exports = function (vArg) {
   if(!Array.isArray) {
--- test/nn.js
+++ test/nn.js
@@ -1,9 +1,9 @@
-'use strict'
-var test = require('tap').test
-var nn = require('../lib/modules/nn')
+'use strict';
+var test = require("tap").test
+var nn = require('../lib/modules/nn');
 
 test('Get Note Number', function(t) {
-  t.equal(nn('C4'), 60)
-  t.equal(nn('C#4'), 61)
-  t.end()
-})
+  t.equal(nn('C4'), 60);
+  t.equal(nn('C#4'), 61);
+  t.end();
+});
